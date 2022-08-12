7acc5b2bf2045e5ab13a1fbb10be82fc00408bde
IIMalysh <iimalysh@mai.education>
2022-08-12 23:27:39 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -6,36 +6,49 @@
 #include <iostream>
 
 
+// ##### config ######
+int sensetivity = 100; //чувствительность
 std::string algorithmName = "FrameDifference"; // выбор алгоритма отделения объектов от фона
+// ##################
 
 
 int main(int argc, char **argv)
 {
+    cv::Mat src, src_backSub, output;
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(13, 13), cv::Point(-1, -1));
+
+
     USBCamera *cam1 = new USBCamera(0);
     cameraAbstraction cameraClient(cam1);
     cameraClient.open();
 
 
-    testHandsDetection *contoursDetection = new testHandsDetection();
+    testHandsDetection *contoursDetection = new testHandsDetection(sensetivity);
     handsDetectionAbstraction contoursDetectionClient(contoursDetection);
 
 
 
-    cv::Mat src, src_bilateral, src_bilateral_backSub, output;
     auto bgs = BGS_Factory::Instance()->Create(algorithmName);
 
     while (1)
     {
         src = cameraClient.getFrame();
         output = src;
+        
 
-        bilateralFilter(src, src_bilateral, 9, 95, 95);
-        // cvtColor(src, src_bilateral, COLOR_BGR2GRAY);
-       src_bilateral_backSub = bgs->apply(src_bilateral);
+        cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+        cv::equalizeHist(src, src);
+        cv::cvtColor(src, src, cv::COLOR_GRAY2BGR);
+        cv::GaussianBlur(src, src, cv::Size(13,13), 0,0);
 
-       contoursDetectionClient.detect(src_bilateral_backSub, output);
+        
+        src_backSub = bgs->apply(src);
+        cv::erode(src_backSub, src_backSub, kernel, cv::Point(-1,-1), 1);
 
-        imshow("outp", output);
+        contoursDetectionClient.detect(src_backSub, output);
+
+        cv::imshow("output", output);
+        cv::imshow("detect", src_backSub);
         cv::waitKey(1);
     }
     return 0;
--- modules/handsDetector.h
+++ modules/handsDetector.h
@@ -35,19 +35,26 @@ private:
 class testHandsDetection : public handsDetectionInterface
 {
 public:
+    testHandsDetection(int inputSensetivity)
+    {
+        sensetivity = inputSensetivity;
+    };
+
     void detect(cv::Mat inputBinaryImage, cv::Mat outputImage)
     {
 
         cv::findContours(inputBinaryImage, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
         for (size_t i = 0; i < contours.size(); i++)
         {
-            int area = 300;
+            int area = sensetivity;
             if (contourArea(contours[i]) > area)
             {
                 cv::Scalar color = cv::Scalar(255, 255, 255, 0);
                 drawContours(outputImage, contours, (int)i, color, 2, cv::LINE_8, hierarchy, 0);
                 // int p = arcLength(contours[i], 1);
                 // approxPolyDP(contours[i], contours[i], 0.03*p, 1);
+
+                std::cout<<contourArea(contours[i])<<std::endl;
             }
         }
     };
@@ -55,4 +62,5 @@ public:
 private:
     std::vector<std::vector<cv::Point>> contours;
     std::vector<cv::Vec4i> hierarchy;
+    int sensetivity = 0;
 };
